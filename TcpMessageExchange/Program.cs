2c808a1ee03b9fd8fb50262e91dce8697e88d438
nacharvs <r3dinc@yandex.ru>
2023-06-23 08:06:19 +0300

    hhhtListener httpClient

---
+++ TcpMessageExchange/Program.cs
@@ -0,0 +1,29 @@
+﻿
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+
+using TcpClient tcpClient = new TcpClient();
+await tcpClient.ConnectAsync("127.0.0.1", 8888);
+
+var words = new string[] { "red", "blue", "green" };
+var stream = tcpClient.GetStream();
+var response = new List<byte>();
+int bytesRead = 10; 
+foreach (var word in words)
+{
+    byte[] data = Encoding.UTF8.GetBytes(word + '\n');
+    await stream.WriteAsync(data);
+    while ((bytesRead = stream.ReadByte()) != '\n')
+    {
+        response.Add((byte)bytesRead);
+    }
+    var translation = Encoding.UTF8.GetString(response.ToArray());
+    Console.WriteLine($"Word {word}: {translation}");
+    response.Clear();
+}
+
+
+await stream.WriteAsync(Encoding.UTF8.GetBytes("END\n"));
+Console.WriteLine("All messages sended");
---
+++ TcpServer/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+var tcpListener = new TcpListener(IPAddress.Any, 8888);
+var words = new Dictionary<string, string>()
+{
+    {"red", "красный" },
+    {"blue", "синий" },
+    {"green", "зеленый" }
+};
+try
+{
+    tcpListener.Start();   
+    Console.WriteLine("Server is Started. Waiting for connections... ");
+
+    while (true)
+    {
+        using var tcpClient = await tcpListener.AcceptTcpClientAsync();
+        var stream = tcpClient.GetStream();
+        var response = new List<byte>();
+        int bytesRead = 10;
+        while (true)
+        {
+            while ((bytesRead = stream.ReadByte()) != '\n')
+            {
+                response.Add((byte)bytesRead);
+            }
+            var word = Encoding.UTF8.GetString(response.ToArray());
+            if (word == "END") break;
+
+            Console.WriteLine($"Request for translation {word}");
+            if (!words.TryGetValue(word, out var translation)) translation = "No data";
+            translation += '\n';
+            await stream.WriteAsync(Encoding.UTF8.GetBytes(translation));
+            response.Clear();
+        }
+    }
+}
+finally
+{
+    tcpListener.Stop();
+}
